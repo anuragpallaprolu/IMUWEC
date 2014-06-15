cc6e7e73f57b4808e23c36857aed0252dd162e48
Anurag Pallaprolu <anurag.pallaprolu@gmail.com>
2014-06-15 20:58:26 +0530

    It Works!

--- py_plot.py
+++ py_plot.py
@@ -5,14 +5,14 @@ from matplotlib import rc, rcParams
 
 # Read in data from an ASCII data table
 data = genfromtxt('gyrox_n.txt')
-#data_f = genfromtxt('gyrox_n_e.txt')
+data_f = genfromtxt('gyrox_n_e.txt')
 #'data' is a matrix containing the columns and rows from the file
 time   = data[:,0]  # Python indices are (row,col) as in linalg
 gyrox = data[:,1]  # Creates arrays for first two columns
-#gyrox_f = data_f[:,1]
+gyrox_f = data_f[:,1]
 # Create a loglog plot of data
 plot(time,gyrox)
-#plot(time,gyrox_f)
+plot(time,gyrox_f)
 # Turn on a grid
 grid(True)
 
